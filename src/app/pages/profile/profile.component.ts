42cc93710e4f9ea9159bf3e56f0778c64add4b85
Nolan Clark <nolanmclark@gmail.com>
2023-12-22 07:43:20 -0600

    Further clean up, new fields

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { AfterViewChecked, Component } from "@angular/core";
+import { MatCheckboxChange } from "@angular/material/checkbox";
 import { BehaviorSubject, Observable, tap } from "rxjs";
 import { AuthenticatedUser } from "src/app/models/admin-user.model";
 import { IPaxUser, PaxUser } from "src/app/models/users.model";
@@ -10,11 +11,11 @@ import { UserAuthenticationService } from "src/app/services/user-authentication.
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements AfterViewChecked {
 
     public authUserData$: Observable<AuthenticatedUser | undefined>;
     public userDataSubject: BehaviorSubject<IPaxUser | undefined> = new BehaviorSubject<IPaxUser | undefined>(undefined);
-    public paxUserData$: Observable<IPaxUser | undefined> = new Observable();
+    public paxUserData$: Observable<IPaxUser | undefined> = this.userDataSubject.asObservable();
 
     public editMode: boolean = false;
 
@@ -23,14 +24,17 @@ export class ProfileComponent {
         private readonly paxManagerService: PaxManagerService
     ) {
         this.authUserData$ = this.userAuthService.authUserData$.pipe(
-            tap((data) => {
+            tap(async (data) => {
                 const paxDataId = data?.getPaxDataId();
                 if (paxDataId && paxDataId !== undefined) {
-                    this.getPaxUserData(paxDataId);
+                    await this.getPaxUserData(paxDataId);
                 }
             })
         );
-        this.paxUserData$ = this.userDataSubject.asObservable();
+    }
+
+    public ngAfterViewChecked() {
+        
     }
 
     public toggleEditMode() {
@@ -42,9 +46,19 @@ export class ProfileComponent {
         this.toggleEditMode();
     }
 
+    public updateHideContactInfoValue($event: MatCheckboxChange, user: IPaxUser) {
+        user.hideContactInformation = $event.checked;
+    }
+
+    public updateActiveStatusValue($event: MatCheckboxChange, user: IPaxUser) {
+        user.activeUser = $event.checked;
+    }
+
     private async getPaxUserData(id: string) {
         const paxData = await (await this.paxManagerService.getDataByAuthId(id)).data();
+        console.log(paxData);
         this.userDataSubject.next(paxData?.toProperties());
     }
+
 
 }
