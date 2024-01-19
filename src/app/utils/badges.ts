befbb01f9a17bb8959f33e0ffa9aabe1b9713961
Nolan Clark <nolanmclark@gmail.com>
2024-01-18 19:43:13 -0600

    More badges

--- src/app/utils/badges.ts
+++ src/app/utils/badges.ts
@@ -20,5 +20,30 @@ export const availableBadges: Array<Badge> = [
         text: 'Site-Q',
         textColor: '#fff',
         backgroundColor: '#ce7900'
+    },
+    {
+        text: 'Site Founder',
+        textColor: '#fff',
+        backgroundColor: 'linear-gradient(73deg, #b90000, #5f5f5f, #0000ff)',
+    },
+    {
+        text: 'Retired Site-Q',
+        textColor: '#7e4900',
+        backgroundColor: '#fdc076'
+    },
+    {
+        text: 'First F',
+        textColor: '#fff',
+        backgroundColor: '#e94b4b'
+    },
+    {
+        text: 'Second F',
+        textColor: '#fff',
+        backgroundColor: '#ff7262'
+    },
+    {
+        text: 'Third F',
+        textColor: '#fff',
+        backgroundColor: '#800080'
     }
 ];
