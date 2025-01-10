0f2b42973c72d44309b063107f75cb4534b99f43
Nolan Clark <nolanmclark@gmail.com>
2025-01-09 23:30:12 -0600

    Test

---
+++ shared/src/types/Workout.ts
@@ -0,0 +1,10 @@
+export enum PreActivity {
+    None = "None",
+    Run = "Run",
+    Murph = "Murph",
+    Smurph = "Smurph",
+    Ruck = "Ruck",
+    Bookclub = "Bookclub",
+    Sandbags = "Sandbags",
+    Other = "Other"
+}
